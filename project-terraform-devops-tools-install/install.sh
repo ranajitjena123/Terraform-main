8f0998c9a85fc1db27b979a80da3483d2db2bb8d
RanjitDevopsEngineer777 <150320418+ranajitjena123@users.noreply.github.com>
2025-01-13 11:12:45 +0530

    Update install.sh

--- project-terraform-devops-tools-install/install.sh
+++ project-terraform-devops-tools-install/install.sh
@@ -26,12 +26,17 @@ sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinu
 sudo yum -y install terraform
 
 #---------------------------------install tomcat------------------
-#sudo wget url https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.83/bin/apache-tomcat-9.0.83.tar.gz
-#sudo tar -xvzf apache-tomcat-9.0.83.tar.gz #untar
-#cd apache-tomcat-9.0.83
-#cd bin
-#chmod +x startup.sh
-
+amazon-linux-extras install java-openjdk11 -y
+wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.98/bin/apache-tomcat-9.0.98.tar.gz
+tar -zxvf apache-tomcat-9.0.98.tar.gz
+sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-9.0.98/conf/tomcat-users.xml
+sed -i '57  a\<role rolename="manager-script"/>' apache-tomcat-9.0.98/conf/tomcat-users.xml
+sed -i '58  a\<user username="tomcat" password="litu" roles="manager-gui, manager-script"/>' apache-tomcat-9.0.98/conf/tomcat-users.xml
+sed -i '59  a\</tomcat-users>' apache-tomcat-9.0.98/conf/tomcat-users.xml
+sed -i '56d' apache-tomcat-9.0.98/conf/tomcat-users.xml
+sed -i '21d' apache-tomcat-9.0.98/webapps/manager/META-INF/context.xml
+sed -i '22d'  apache-tomcat-9.0.98/webapps/manager/META-INF/context.xml
+sh apache-tomcat-9.0.98/bin/startup.sh
 
 
 #---------------------------Maven install -------------
